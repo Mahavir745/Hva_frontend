556c9bae10470b6ec67a658b46755e7ffb73fe8c
Mahavir745 <mahavir777kumar@gmail.com>
2024-07-30 20:05:48 +0530

    task 21 calculate area

---
+++ JavaScript/21_calculate_area/script.js
@@ -0,0 +1,33 @@
+//todo: Calculate Area:
+
+function calculateArea (width,height){
+  return width * height
+}
+
+console.log(calculateArea(5,10));
+
+// todo: Modify calculate area function with default values:
+
+function calculateAreaWithDefaults(width=1,height=1){
+  return width*height
+}
+
+console.log(calculateAreaWithDefaults());
+
+// todo: Rewrite calculate area as a function expression:
+
+let calculateAreaFunction = function calculateArea(width,height){
+    return width*height
+}
+
+console.log(calculateAreaFunction(6,7));
+
+// todo: Rewrite calculate area as an arrow function:
+
+let calculateAreaArrow = (width,height)=>{
+  return width*height
+}
+
+
+
+console.log(calculateAreaArrow(5,10));
