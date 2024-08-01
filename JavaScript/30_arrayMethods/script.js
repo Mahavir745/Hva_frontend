8b863892d33c6885af05745da7f240fb32332643
Mahavir745 <mahavir777kumar@gmail.com>
2024-08-01 16:26:48 +0530

    task 30 array methods foreach,map,filter

---
+++ JavaScript/30_arrayMethods/script.js
@@ -0,0 +1,43 @@
+// //todo: Define and initialize the Array:
+
+// let arr = [1,2,3,4,5,6,7]
+
+
+// //todo: lterate and log using forEach()
+// arr.forEach((ele)=>{
+//   console.log(ele);
+// })
+
+// arr.forEach((ele)=>{
+//   console.log(ele*2);
+// })
+
+// //todo: iterate and create a new modified array using map():
+
+// let squareNumbers = [1,4,9,16,25,36,49,64,81,100]
+
+// squareNumbers.map(function(ele){
+//   console.log(ele);
+// })
+
+// squareNumbers.map((el)=>{
+//   console.log(el);
+// })
+
+//todo: Iterate and create a new filtered array using filter():
+
+// let evenNumbers = [2,4,6,8,9,12,14]
+
+// let newarr = evenNumbers.filter(function(ele){
+//   return ele%2==0
+// })
+
+// console.log(newarr);
+
+
+let evenNumbers = [2,4,6,8,12,14]
+
+evenNumbers.filter((even)=>{
+  console.log(even);
+})
+
