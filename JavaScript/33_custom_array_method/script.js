6aaddca90264f3389b78bbdc6b19f035d80e5436
Mahavir745 <mahavir777kumar@gmail.com>
2024-08-05 12:10:40 +0530

    task 33 custom array

--- JavaScript/33_custom_array_method/script.js
+++ JavaScript/33_custom_array_method/script.js
@@ -1,26 +1,74 @@
 //todo: forEachArray Function:
 
+// function forEachone (arr, callback){
+//   for(let i=0; i<arr.length; i+=1){
+//     callback(arr[i],i,arr)
+//   }
+// }
 
-function forEach(arr, callback){
-  for(let i =0; i<arr.length; i++){
-    callback(arr[i],i,arr)
+// function forEachcall(ele,index,arr){
+//   console.log(ele, index, arr );
+  
+// }
+
+// let arr = [1,2,3,4,5,6]
+
+// forEachone(arr,forEachCall)
+
+// todo: map function:
+
+
+// let output = []
+
+// function mapFunction(arr,callback){
+
+//   for(let i=0; i<arr.length; i++){
+//      let data = callback(arr[i],i,arr)
+//      output.push(data)
+//   }
+
+//   return output
+// }
+
+// function mapCall(ele,index,arr){
+//   return arr
+  
+// }
+
+// let arr = [1,2,3,4,5,6]
+
+// let mapOutput = mapFunction(arr,mapCall)
+// console.log(mapOutput);
+
+
+//todo: filter 
+
+let result = []
+
+function ownfilter(arr,callback){
+  for(let i=0; i<arr.length; i++){
+
+    if(callback(arr[i])){
+      result.push(arr[i])
+    }
   }
+
+  return result
 }
 
+function callfilter(ele){
+  return ele>18
+}
 
-let arr = [1,2,3,4,5,5]
+let arr = [12,23,12,32,12,13,43]
+let outputNewFilter = ownfilter(arr,callfilter)
 
-forEach(arr, (ele)=>{
-  console.log(ele);
-  
-})
-
-
-
+console.log(outputNewFilter);
 
 
 
-```
+
+
 // Array.prototype.forEachonefunction=function(callback){
 //   for(let i =0; i< this.length; i++){
 //     callback(this[i], i, this)
@@ -47,4 +95,4 @@ forEach(arr, (ele)=>{
 // arr.forEachTwofunction((ele)=>{
 //   console.log(ele);
 // })
-```
+
